76549c06f740b3325d80a4ced4dff503f9506b89
zuisong <378818573@qq.com>
2018-03-15 15:04:55 +0800

    更新两个大数相乘的代码

--- src/main/kotlin/cn/mmooo/Q151_Multiply_Strings.kt
+++ src/main/kotlin/cn/mmooo/Q151_Multiply_Strings.kt
@@ -25,36 +25,37 @@ Both num1 and num2 contains only digits 0-9.
 Both num1 and num2 does not contain any leading zero.
 You must not use any built-in BigInteger library or convert the inputs to integer directly.
  */
+val plusTwoIntArray = { num1: IntArray, num2: IntArray ->
+    num1.reverse()
+    num2.reverse()
+    var jinwei = 0
+    val result = IntArray(222, { -1 })
+    val maxIndex = Math.max(num1.indexOfLast { it > 0 }, num2.indexOfLast { it > 0 })
+    (0..maxIndex).forEach {
+        val sum = num1.getOrElse(it) { 0 } + num2.getOrElse(it) { 0 } + jinwei
+        jinwei = sum.div(10)
+        result[it] = sum.rem(10)
+    }
+    result[maxIndex + 1] = jinwei
+
+    result
+            .copyOfRange(0, result.indexOfLast { it > 0 } + 1)
+            .reversed()
+            .let {
+                if (it.isNotEmpty()) {
+                    it.toIntArray()
+                } else {
+                    IntArray(1, { 0 })
+                }
+            }
+
+}
+
 fun multiply(num1: String, num2: String): String {
 
     fun String.toIntArray() =
             this.map(Character::getNumericValue).toIntArray()
 
-    val plusTwoIntArray = { num1: IntArray, num2: IntArray ->
-        num1.reverse()
-        num2.reverse()
-        var jinwei = 0
-        val result = IntArray(222, { -1 })
-        val maxIndex = Math.max(num1.indexOfLast { it > 0 }, num2.indexOfLast { it > 0 })
-        (0..maxIndex).forEach {
-            val sum = num1.getOrElse(it) { 0 } + num2.getOrElse(it) { 0 } + jinwei
-            jinwei = sum.div(10)
-            result[it] = sum.rem(10)
-        }
-        result[maxIndex + 1] = jinwei
-
-        result
-                .copyOfRange(0, result.indexOfLast { it > 0 } + 1)
-                .reversed()
-                .let {
-                    if (it.isNotEmpty()) {
-                        it.toIntArray()
-                    } else {
-                        IntArray(1, { 0 })
-                    }
-                }
-
-    }
 
     val intArrayMutiInt = { arr: IntArray, n: Int ->
         var sum = IntArray(1)
