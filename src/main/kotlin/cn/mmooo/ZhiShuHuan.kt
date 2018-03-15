e6527ac1f724edfe6922576f50056294b7691bc9
zuisong <378818573@qq.com>
2018-03-15 16:35:59 +0800

    完善质数环算法

--- src/main/kotlin/cn/mmooo/ZhiShuHuan.kt
+++ src/main/kotlin/cn/mmooo/ZhiShuHuan.kt
@@ -1,39 +1,49 @@
-const val numberSize = 8
+import kotlin.system.measureTimeMillis
+
 
 fun main(args: Array<String>) {
+    measureTimeMillis {
+        zhishuhuan(16)
+                .let { println(it) }
+    }.let { println("耗时 $it ms") }
 
+}
 
-    val zhishuList =
-            (2 until numberSize * 2)
+/**
+ *  质数环: 1 到 [numberSize]  按某个顺序这些数排成一个环，要求相邻的两个数相加为质数
+ *  @return 一共有多少种排列方法
+ */
+fun zhishuhuan(numberSize: Int): Int {
+    val zhishus =
+            (4 until numberSize * 2)
                     .filter {
-                        it <= 3 || (2..Math.sqrt(1.0 * it).toInt())
+                        (2..Math.sqrt(1.0 * it).toInt())
                                 .none { i -> it % i == 0 }
-                    }
-    println(zhishuList)
-    println()
+                    }.plus(arrayOf(2, 3)).toHashSet()
 
     fun Int.isZhiShu(): Boolean =
-            zhishuList.contains(this)
+            zhishus.contains(this)
+
+    var count = 0
 
     fun solve(index: Int, arr: IntArray) {
-        (1..numberSize)
-                .filter { !arr.copyOfRange(0, index).contains(it) }
+        (1..numberSize).filter { !arr.copyOfRange(0, index).contains(it) }
                 .filter {
                     index == 0 || (it + arr[index - 1]).isZhiShu()
                 }.forEach {
                     arr[index] = it
                     if (index < numberSize - 1) {
                         solve(index + 1, arr)
-                    }
-                    if (index == numberSize - 1 && (it + arr[0]).isZhiShu()) {
-                        println(arr.toList())
-//                        return
+                    } else if (index == numberSize - 1 && (it + arr[0]).isZhiShu()) {
+                        count++
+//                        arr.toList()
+                        return
                     }
                 }
     }
 
     val array = IntArray(numberSize)
     array[0] = 1
-    System.currentTimeMillis()
     solve(1, array)
+    return count
 }
