e3bed22fcbd802cee4735e2fd46f13d43e3037c1
zuisong <378818573@qq.com>
2018-03-16 10:02:03 +0800

    增加注释, 删除没有实现的代码

--- src/main/kotlin/cn/mmooo/Q53_Maximum_Subarray.kt
+++ src/main/kotlin/cn/mmooo/Q53_Maximum_Subarray.kt
@@ -1,50 +1,5 @@
 package cn.mmooo.q53
 
-private class Solution {
-    fun maxSubArray(nums: IntArray): Int {
-
-        val max = nums.max()!!
-        if (max <= 0) return max
-        var maxSum = 0
-
-        var sum = 0
-
-        nums.forEach {
-            sum += it
-            if (sum < 0) {
-                sum = 0
-            }
-            if (sum > maxSum) {
-                maxSum = sum
-            }
-
-        }
-        return maxSum
-    }
-
-    fun maxSubArray2(nums: IntArray): Int {
-
-        val max = nums.max()!!
-        if (max <= 0) return max
-        var maxSum = 0
-
-        var sum = 0
-
-        nums.forEach {
-            sum += it
-            if (sum < 0) {
-                sum = 0
-            }
-            if (sum > maxSum) {
-                maxSum = sum
-            }
-
-        }
-        return maxSum
-    }
-
-
-}
 
 /**
 Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
@@ -57,3 +12,37 @@ fun main(args: Array<String>) {
             .let { println(it) }
 
 }
+
+/**
+ * 最大子序列和问题: 从一个数组找出连续的一串，使得这一串的和是所有字串中最大的
+ */
+private class Solution {
+    /**
+     * O(log(n))复杂度算法，最优解
+     */
+    fun maxSubArray(nums: IntArray): Int {
+        val max = nums.max()!!
+        if (max <= 0) return max
+        var maxSum = 0
+        var sum = 0
+        nums.forEach {
+            sum += it
+            if (sum < 0) {
+                sum = 0
+            }
+            if (sum > maxSum) {
+                maxSum = sum
+            }
+        }
+        return maxSum
+    }
+
+    /**
+     * 分治解法
+     */
+    fun maxSubArray2(nums: IntArray): Int {
+        TODO("待实现")
+    }
+
+}
+
