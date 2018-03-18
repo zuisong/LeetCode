c297ec2e1ac7369b908ca068a6a06f8df14b6adb
ZuiSong <378818573@qq.com>
2018-03-18 19:09:09 +0800

    新增将一个数插入到已经排好序的数组中,寻找插入位置的算法

--- src/main/kotlin/cn/mmooo/Q35_Search_Insert_Position.kt
+++ src/main/kotlin/cn/mmooo/Q35_Search_Insert_Position.kt
@@ -1,19 +1,21 @@
 package cn.mmooo.q35
 
-
-private inline val Int.left get() = 2 * this + 1
-private inline val Int.right get() = 2 * this + 2
-
 private class Solution {
-    fun searchInsert(nums: IntArray, target: Int): Int {
-
-        return nums.binarySearch(target).let {
-            when {
-                it < 0 -> -1 * it - 1
-                else -> it
+    fun searchInsert(nums: IntArray, target: Int, minIndex: Int = 0, maxIndex: Int = nums.lastIndex): Int {
+        return when {
+            nums[minIndex] >= target -> minIndex
+            nums[maxIndex] == target -> maxIndex
+            nums[maxIndex] < target -> maxIndex + 1
+            minIndex + 1 == maxIndex -> minIndex + 1
+            else -> {
+                val mid = (maxIndex + minIndex) / 2
+                when {
+                    target == nums[mid] -> mid
+                    target > nums[mid] -> searchInsert(nums, target, mid, maxIndex)
+                    else -> searchInsert(nums, target, minIndex, mid)
+                }
             }
         }
-
     }
 }
 
@@ -24,7 +26,7 @@ If not, return the index where it would be if it were inserted in order.
 You may assume no duplicates in the array.
  */
 fun main(args: Array<String>) {
-    Solution().searchInsert(intArrayOf(1, 3, 5, 6), 3)
+    Solution().searchInsert(intArrayOf(1, 3, 5, 6), 0)
             .let {
                 println(it)
             }
