142041dcf263514cab79c78e9cdc242fb6b00065
ZuiSong <378818573@qq.com>
2018-03-18 19:33:50 +0800

    更新最大子串和问题的分支思想解法

--- src/main/kotlin/cn/mmooo/Q53_Maximum_Subarray.kt
+++ src/main/kotlin/cn/mmooo/Q53_Maximum_Subarray.kt
@@ -9,7 +9,9 @@ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
  */
 fun main(args: Array<String>) {
     Solution().maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4))
-            .let { println(it) }
+            .let { println("最优时间复杂度算法的解是: $it") }
+    Solution().maxSubArray2(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4))
+            .let { println("分治思想实现的解是: $it") }
 
 }
 
@@ -18,7 +20,7 @@ fun main(args: Array<String>) {
  */
 private class Solution {
     /**
-     * O(log(n))复杂度算法，最优解
+     * O(n)复杂度算法，最优解
      */
     fun maxSubArray(nums: IntArray): Int {
         val max = nums.max()!!
@@ -38,10 +40,42 @@ private class Solution {
     }
 
     /**
-     * 分治解法
+     * 分治算法
+     * 分成左右两块求解，最大的子序列和肯定出现在 左边、右边或者在穿越左右两块的一个子串
+     * 在左右两边的相当于第一个问题的一个子问题，重点在于求跨越两边的最大子串
+     * 看代码
      */
-    fun maxSubArray2(nums: IntArray): Int {
-        TODO("待实现")
+    fun maxSubArray2(nums: IntArray, startIndex: Int = 0, endIndex: Int = nums.lastIndex): Int {
+        if (endIndex <= startIndex) {
+            return nums[startIndex]
+        }
+
+        val mid = (startIndex + endIndex) / 2
+
+
+        var i = mid
+        var maxOfLeft = 0
+        var sumOfLeft = 0
+        while (i >= startIndex) {
+            sumOfLeft += nums[i]
+            if (sumOfLeft > maxOfLeft) {
+                maxOfLeft = sumOfLeft
+            }
+            i--
+        }
+        var maxOfRight = 0
+        var sumOfRight = 0
+        var j = mid + 1
+        while (j <= endIndex) {
+            sumOfRight += nums[j]
+            if (sumOfRight > maxOfRight) {
+                maxOfRight = sumOfRight
+            }
+            j++
+        }
+        var maxCrossMid = maxOfLeft + maxOfRight
+
+        return maxOf(maxSubArray2(nums, startIndex, mid - 1), maxSubArray2(nums, mid + 1, endIndex), maxCrossMid)
     }
 
 }
