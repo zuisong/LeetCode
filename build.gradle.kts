f8b602d65657df6972e207b2d917fe5a4ed16712
zuisong <378818573@qq.com>
2018-08-28 17:46:54 +0800

    update pom

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
-    kotlin("jvm") version "1.2.40"
+    kotlin("jvm") version "1.2.51"
 }
 
 
 dependencies {
     compile(kotlin("stdlib"))
     compile(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "0.22.5")
+    compile(group = "org.projectlombok", name = "lombok", version = "1.16.20")
 }
 
 repositories {
     jcenter()
+    mavenLocal()
 }
